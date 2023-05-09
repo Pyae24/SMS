fbec11993bf220be927be674dfeebb051559e5af
Circle <phonepyaepresent@gmail.com>
2023-05-09 22:13:41 +0630

    Tuesday Commit

---
+++ src/main/java/com/example/student_management_system/LecturerLogin.java
@@ -0,0 +1,18 @@
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LecturerLogin", value = "/LecturerLogin")
+public class LecturerLogin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
